2ed4dc70c0915f21b18e65bf15c7ebe759a9bb47
DS0708 <tlsehdtjd070@naver.com>
2024-02-21 21:06:23 +0900

    3.10 일봉 얻어오기

--- api/Kiwoom.py
+++ api/Kiwoom.py
@@ -1,6 +1,8 @@
 from PyQt5.QAxContainer import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+import time
+import pandas as pd
 
 
 class Kiwoom(QAxWidget):
@@ -13,6 +15,8 @@ class Kiwoom(QAxWidget):
 
         self.account_number = self.get_account_number() # 계좌번호 저장
 
+        self.tr_event_loop = QEventLoop() #tr요청에 대한 응답 대기를 위한 변수
+
     def _make_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
@@ -20,6 +24,8 @@ class Kiwoom(QAxWidget):
         """API로 보내는 요청들을 받아올 slot을 등록하는 함수"""
         # 로그인 응답의 결과를 _on_login_connect을 통해 받도록 설정
         self.OnEventConnect.connect(self._login_slot)
+        # TR의 응답 결과를 _on_receive_tr_data를 통해 받도록 설정
+        self.OnReceiveTrData.connect(self._on_receive_tr_data)
 
     def _login_slot(self, err_code):
         if err_code == 0:
@@ -52,4 +58,56 @@ class Kiwoom(QAxWidget):
         code_name = self.dynamicCall("GetMasterCodeName(QString)", code)
         return code_name
 
+    def get_price_data(self, code):     #종목의 상장일부터 가장 최근 일자까지 일봉 정보를 가져오는 함수
+        self.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+        self.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "1")
+        self.dynamicCall("CommRqData(QString, QString, int, QString)", "opt10081_req", "opt10081", 0, "0001")
 
+        self.tr_event_loop.exec_()
+
+        ohlcv = self.tr_data
+
+        while self.has_next_tr_data:
+            self.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+            self.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "1")
+            self.dynamicCall("CommRqData(QString, QString, int, QString)", "opt10081_req", "opt10081", 2, "0001")
+            self.tr_event_loop.exec_()
+
+            for key, val in self.tr_data.items():   # 이미 받아온 데이터의 마지막 부분에 붙이는 코드
+                ohlcv[key] += val
+
+        df = pd.DataFrame(ohlcv, columns=['open', 'high', 'low', 'close', 'volume'], index=ohlcv['date'])
+
+        return df[::-1] #index를 기준으로 오름차순 정렬하여 return, 내림차순은 return df
+
+    def _on_receive_tr_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unused4):
+        "TR조회의 응답 결과를 얻어오는 함수"
+        print("[Kiwoom] _on_receive_tr_data is called {} / {} / {}".format(screen_no, rqname, trcode))
+        tr_data_cnt = self.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
+
+        if next == '2':
+            self.has_next_tr_data = True
+        else:
+            self.has_next_tr_data = False
+
+        if rqname == "opt10081_req":
+            ohlcv = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}
+
+            for i in range(tr_data_cnt):
+                date = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "일자")
+                open = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "시가")
+                high = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "고가")
+                low = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "저가")
+                close = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "현재가")
+                volume = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "거래량")
+
+                ohlcv['date'].append(date.strip())
+                ohlcv['open'].append(int(open))
+                ohlcv['high'].append(int(high))
+                ohlcv['low'].append(int(low))
+                ohlcv['close'].append(int(close))
+                ohlcv['volume'].append(int(volume))
+
+            self.tr_data = ohlcv
+        self.tr_event_loop.exit()
+        time.sleep(0.5)
--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import sys
 app = QApplication(sys.argv)
 Kiwoom = Kiwoom()
 
-kospi_code_list = Kiwoom.get_code_list_by_market("0")
-print(kospi_code_list)
-for code in kospi_code_list:
-    code_name = Kiwoom.get_master_code_name(code)
-    print(code,code_name)
+# kospi_code_list = Kiwoom.get_code_list_by_market("0")
+# print(kospi_code_list)
+# for code in kospi_code_list:
+#     code_name = Kiwoom.get_master_code_name(code)
+#     print(code,code_name)
+#
+# kosdaq_code_list = Kiwoom.get_code_list_by_market("10")
+# print(kosdaq_code_list)
+# for code in kosdaq_code_list:
+#     code_name = Kiwoom.get_master_code_name(code)
+#     print(code,code_name)
 
-kosdaq_code_list = Kiwoom.get_code_list_by_market("10")
-print(kosdaq_code_list)
-for code in kosdaq_code_list:
-    code_name = Kiwoom.get_master_code_name(code)
-    print(code,code_name)
-
+df = Kiwoom.get_price_data("005930")
+print(df)
 app.exec_()
 
