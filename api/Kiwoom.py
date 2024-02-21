407fb3be1405b8e7e1c1bc9ac1a67733583d3ddf
DS0708 <tlsehdtjd070@naver.com>
2024-02-21 09:10:41 +0900

    3.9 종목 얻어오기

--- api/Kiwoom.py
+++ api/Kiwoom.py
@@ -11,6 +11,8 @@ class Kiwoom(QAxWidget):
         self._set_signal_slots()        # 로그인, 실시간 정보, 기타 제공받을 수 있는 데이터에 대한 응답을 받을 수 있는 slot함수들을 등록
         self._comm_connect()            # 로그인 요청 보내기, _login_slot에서 응답을 받음
 
+        self.account_number = self.get_account_number() # 계좌번호 저장
+
     def _make_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
@@ -34,5 +36,20 @@ class Kiwoom(QAxWidget):
         # 로그인 요청에 대한 응답이 올때까지 대기
         self.login_event_loop = QEventLoop()
         self.login_event_loop.exec_()
+
+    def get_account_number(self, tag="ACCNO"): # 계좌번호를 의미하는 "ACCNO", tag에 따라 얻어 오는 값 변경
+        account_list = self.dynamicCall("GetLoginInfo(QString)", tag)  # tag로 전달한 요청에 대한 응답을 받아옴
+        account_number = account_list.split(';')[0]   # 국내 주식 모의투자만 신청했을때를 가정, 계좌가 한 개임을 보장
+        print(account_number)
+        return account_number
+
+    def get_code_list_by_market(self, market_type): # 주식 시장별 종목 코드 리스트 뽑기 ,market_type은 어떤 시장에서 종목을 얻어 올지 의미하는 구분 값
+        code_list = self.dynamicCall("GetCodeListByMarket(QString)", market_type)
+        code_list = code_list.split(';')[:-1]   # 마지막 항목에는 ''이 붙어 있으므로 이것을 제외하고 넣기
+        return code_list
+
+    def get_master_code_name(self, code): #종목 코드 리스트를 종목명으로 반환
+        code_name = self.dynamicCall("GetMasterCodeName(QString)", code)
+        return code_name
 
 
